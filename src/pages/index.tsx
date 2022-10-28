267bb712c48ae428bf623aeff5bedf88833920e3
Joshua Richardson <josh@cursedtale.com>
2022-10-28 11:03:26 -0700

    Add touch listeners for testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useEffect } from "react";
 
 import {
   CurrentWord,
@@ -11,23 +12,35 @@ import {
   TopAside,
 } from "../components";
 
-const Home: NextPage = () => (
-  <>
-    <Head>
-      <title>Boogl</title>
-      <meta name="description" content="Interview Coding Sample" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home: NextPage = () => {
+  useEffect(() => {
+    const touchHandler = (e: TouchEvent) => console.log(e);
+    window.addEventListener("touchstart", touchHandler);
+    window.addEventListener("touchmove", touchHandler);
 
-    <main className="container mx-auto flex min-h-screen items-center justify-center gap-4 p-4 text-neutral-100">
-      <div className="flex max-w-sm flex-1 flex-col gap-6">
-        <h1 className="text-center text-5xl">Boogl</h1>
-        <TopAside />
-        <Game />
-        <CurrentWord />
-        <WordList />
-      </div>
-    </main>
-  </>
-);
+    return () => {
+      window.removeEventListener("touchstart", touchHandler);
+      window.removeEventListener("touchmove", touchHandler);
+    };
+  }, []);
+  return (
+    <>
+      <Head>
+        <title>Boogl</title>
+        <meta name="description" content="Interview Coding Sample" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container mx-auto flex min-h-screen items-center justify-center gap-4 p-4 text-neutral-100">
+        <div className="flex max-w-sm flex-1 flex-col gap-6">
+          <h1 className="text-center text-5xl">Boogl</h1>
+          <TopAside />
+          <Game />
+          <CurrentWord />
+          <WordList />
+        </div>
+      </main>
+    </>
+  );
+};
 export default Home;
