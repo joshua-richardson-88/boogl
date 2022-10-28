f10787a8463484be513714fe81f29e2caba08d0a
Joshua Richardson <josh@cursedtale.com>
2022-10-28 00:12:51 -0700

    Actually remove InfoPanel

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 import Cell from "./Cell";
 import CurrentWord from "./CurrentWord";
 import Game from "./Game";
-import InfoPanel from "./InfoPanel";
 import NewGameButton from "./NewGameButton";
 import LeftAside from "./LeftAside";
 import RightAside from "./RightAside";
@@ -12,7 +11,6 @@ export {
   Cell,
   CurrentWord,
   Game,
-  InfoPanel,
   NewGameButton,
   LeftAside,
   RightAside,
