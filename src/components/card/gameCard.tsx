a51c517f19d3f0b09de38362327664d456f1013d
Joshua Richardson <josh@cursedtale.com>
2022-11-04 16:39:13 -0700

    Add score to each game in profile page

--- src/components/card/gameCard.tsx
+++ src/components/card/gameCard.tsx
@@ -1,16 +1,21 @@
+import { calculateScore } from "../../utils/gameUtils";
 import type { Game } from "../../utils/userStore";
+
+const reduceScore = (as: string[]) =>
+  as.reduce((a, v) => a + calculateScore(v), 0);
 
 const GameCard = ({ type, date, tiles, words, heatMap }: Game) => {
   return (
     <div className="flex w-full max-w-sm flex-col gap-2 rounded-lg border border-gray-200 bg-gray-200 p-4 text-neutral-100 shadow-md dark:border-gray-700 dark:bg-gray-700">
       <div className="flex justify-between">
-        <div>
+        <div className="flex flex-col">
           <h2 className="font-bold">
             {type === "solo" ? "Solo Play" : "Multiplayer"}
           </h2>
-          <span className="text-neutral-400">
-            {new Date(date).toLocaleString()}
-          </span>
+          <p className="text-neutral-400">{new Date(date).toLocaleString()}</p>
+          <p className="flex flex-1 items-center pl-4 text-4xl">
+            {reduceScore(words)}
+          </p>
         </div>
         <div className="grid w-fit select-none grid-cols-4 grid-rows-4 gap-px">
           {tiles.map((tile, i) => {
