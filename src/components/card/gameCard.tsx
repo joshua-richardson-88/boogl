1bc15d6e03ed50e6fe8bd4b01488504f686a9623
Joshua Richardson <josh@cursedtale.com>
2022-11-06 01:52:12 -0700

    pretty up game card

--- src/components/card/gameCard.tsx
+++ src/components/card/gameCard.tsx
@@ -9,13 +9,11 @@ const GameCard = ({ type, date, tiles, words, heatMap }: Game) => {
     <div className="flex w-full max-w-sm flex-col gap-2 rounded-lg border border-gray-200 bg-gray-200 p-4 text-neutral-100 shadow-md dark:border-gray-700 dark:bg-gray-700">
       <div className="flex justify-between">
         <div className="flex flex-col">
-          <h2 className="font-bold">
+          <h2 className="flex flex-1 text-4xl">{reduceScore(words)}</h2>
+          <p className="font-bold">
             {type === "solo" ? "Solo Play" : "Multiplayer"}
-          </h2>
-          <p className="text-neutral-400">{new Date(date).toLocaleString()}</p>
-          <p className="flex flex-1 items-center pl-4 text-4xl">
-            {reduceScore(words)}
           </p>
+          <p className="text-neutral-400">{new Date(date).toLocaleString()}</p>
         </div>
         <div className="grid w-fit select-none grid-cols-4 grid-rows-4 gap-px">
           {tiles.map((tile, i) => {
@@ -42,11 +40,11 @@ const GameCard = ({ type, date, tiles, words, heatMap }: Game) => {
       {words.length > 0 && (
         <div>
           <h3 className="mb-2 border-b font-bold">Words found</h3>
-          <p>
+          <p className="text-neutral-400">
             {words
               .filter((w) => w.length > 2)
               .sort((a, b) => b.length - a.length)
-              .join(", ")}
+              .join(" • ")}
           </p>
         </div>
       )}
